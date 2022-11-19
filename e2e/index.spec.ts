ee1e25de14c2423b0329f4b4cd568b17122b5c00
makotot <makoto.ttn@gmail.com>
2022-11-20 00:56:53 +0900

    add e2e

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,9 +1,18 @@
 import { test, expect } from "@playwright/test"
+import { getPosts } from "../utils/getPosts"
 
 test.describe("/", async () => {
   test("should have a link to home.", async ({ page }) => {
     await page.goto("/")
     const headerTitleLink = page.getByRole("link", { name: "M" })
     await expect(headerTitleLink).toHaveAttribute("href", "/")
+  })
+
+  test("should have a link to a blog post.", async ({ page }) => {
+    await page.goto("/")
+    const posts = getPosts()
+    await page.click(`text=${posts[0].data.title}`)
+    await expect(page).toHaveURL(`/posts/${posts[0].slug}`)
+    await expect(page.locator("h1")).toContainText(posts[0].data.title)
   })
 })
