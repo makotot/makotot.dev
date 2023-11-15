5811a77b58d39535bc629b5e67cb67eace373693
makotot <makoto.ttn@gmail.com>
2023-11-16 02:09:58 +0900

    use ts config

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography'
+import daisyui from 'daisyui'
+
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [
+    typography,
+    daisyui,
+  ],
+  daisyui: {
+    themes: ["forest", "emerald"],
+  },
+} satisfies Config
+
+export default config
