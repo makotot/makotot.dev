3435f3e12dc0aaab1c21272cc205bedc11807a7a
makotot <makoto.ttn@gmail.com>
2023-11-16 02:10:20 +0900

    format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,13 @@
-import type { Config } from 'tailwindcss'
-import typography from '@tailwindcss/typography'
-import daisyui from 'daisyui'
+import type { Config } from "tailwindcss"
+import typography from "@tailwindcss/typography"
+import daisyui from "daisyui"
 
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
-  plugins: [
-    typography,
-    daisyui,
-  ],
+  plugins: [typography, daisyui],
   daisyui: {
     themes: ["forest", "emerald"],
   },
