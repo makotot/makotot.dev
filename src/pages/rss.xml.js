d4fa2a0c83c58d86ded4131b87ed254c0c3ff9d6
makotot <makoto.ttn@gmail.com>
2024-01-17 23:44:53 +0900

    fix xml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -11,6 +11,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
+      pubDate: post.data.date,
       link: `/posts/${post.slug}/`,
     })),
   })
