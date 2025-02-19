89112406005a4d55ee6a8ec8d7a97d0ed7b294ff
makotot <makoto.ttn@gmail.com>
2025-02-19 22:42:52 +0900

    add zenn

--- src/features/Posts/components/Posts/index.tsx
+++ src/features/Posts/components/Posts/index.tsx
@@ -1,14 +1,18 @@
 import { posts } from '#site/content';
 import Link from 'next/link';
 import { sva } from 'styled-system/css';
+import { dateISO8601 } from '../../formatter/dateISO8601';
 
 export function Posts() {
   const styles = style();
+
   return (
     <ol className={styles.root}>
       {getPublishedPosts().map((post) => (
         <li key={post.title} className={styles.item}>
-          <time className={styles.time}>{post.date}</time>
+          <time className={styles.time} dateTime={dateISO8601(post.date)}>
+            {post.date}
+          </time>
           <Link href={post.path} className={styles.link}>
             {post.title}
           </Link>
