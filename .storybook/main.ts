67881d38ea6675c46b34e69fb159a979a5ea0e7f
makotot <makoto.ttn@gmail.com>
2025-11-06 06:47:35 +0900

    fix format

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 // This file has been automatically migrated to valid ESM format by Storybook.
-import { fileURLToPath } from "node:url";
-import { dirname } from "node:path";
-import { createRequire } from "node:module";
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { createRequire } from 'node:module';
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const __filename = fileURLToPath(import.meta.url);
